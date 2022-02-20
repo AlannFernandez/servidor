392717d66b92f69b3c993ff399673fa4869fb60f
Alan <alanfernande8@gmail.com>
2022-02-20 12:39:29 -0300

    fix file keys.js

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,7 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host: 'bflcaq4yqm0axd8usdvh-mysql.services.clever-cloud.com',//
-        user: 'roouckuxtm4dbnfg0f9t',
+        user: 'uckuxtm4dbnfg0f9',
         password: 'd8FKgP9dnM9kjzndbftT',
         database: 'bflcaq4yqm0axd8usdvh'
     }
