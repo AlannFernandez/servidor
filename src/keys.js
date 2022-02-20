1401442a2cb6c837d32cca56e9c66476964134fc
Alan <alanfernande8@gmail.com>
2022-02-20 12:01:39 -0300

    config db

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'apirest'
+        host: 'bflcaq4yqm0axd8usdvh-mysql.services.clever-cloud.com',//
+        user: 'roouckuxtm4dbnfg0f9t',
+        password: 'd8FKgP9dnM9kjzndbftT',
+        database: 'bflcaq4yqm0axd8usdvh'
     }
 };
